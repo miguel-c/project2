bce793316091fd326170c1e711d29c6818576210
miguel-c <miguel-c@users.noreply.github.com>
2016-10-05 15:21:48 -0400

    Run new parentChild tests

---
+++ testData/run_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python3 ../FamilyTree_CorteReal.py < CRAltFamily.txt > CRAltFamily_out.txt
+python3 ../FamilyTree_CorteReal.py < parentChild1.txt > parentChild1_out.txt
+python3 ../FamilyTree_CorteReal.py < parentChild2.txt > parentChild2_out.txt
